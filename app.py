8d53642bf55f6762496e21b729df92500699238a
Khalifa Fadel <fffadel19982@gmail.com>
2023-08-24 16:49:01 +0100

    erf

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world!"
+    return "Hello, world! the world is good"
 
 if __name__ == '__main__':
     app.run(
