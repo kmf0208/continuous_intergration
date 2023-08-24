80a4ab4e5d0fa2aee0be715d005be661621126bd
Khalifa Fadel <fffadel19982@gmail.com>
2023-08-24 16:53:33 +0100

    should pass

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_get_home(web_client):
     response = web_client.get("/")
     assert response.status_code == 200
-    assert response.data.decode("utf-8") == "Hello, world!"
+    assert response.data.decode("utf-8") == "Hello, world! the world is good"
